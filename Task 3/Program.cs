ccb7125f9ad01992404646092d9b81d0ac2f206f
Stanislav <S@mail.ru>
2022-12-27 21:32:16 +0300

    add Task 3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -4,11 +4,11 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите натуральное число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int AkkermanFunction (int m, int n)
+int AckermanFunction (int m, int n)
 {
     if (m == 0) return n + 1;
-    if (m != 0 && n == 0) return AkkermanFunction(m - 1, 1);
-    if (m > 0 && n > 0) return AkkermanFunction(m - 1, AkkermanFunction (m, n - 1));
-    return AkkermanFunction (m, n);
+    if (m != 0 && n == 0) return AckermanFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction (m, n - 1));
+    return AckermanFunction (m, n);
 }
-Console.Write($"Функция Аккермана для чисел A({m},{n}) = {AkkermanFunction(m, n)}");
+Console.Write($"Функция Аккермана для чисел A({m},{n}) = {AckermanFunction(m, n)}");
