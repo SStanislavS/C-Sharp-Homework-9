96599d99f4e462254dcdc2554c0e47cf33e2e6f4
Stanislav <S@mail.ru>
2022-12-27 19:47:10 +0300

    add Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Программа выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("Введите натуральное число от 1:");
+int number = int.Parse(Console.ReadLine());
+
+void PrintNumberRevers (int number)
+{
+    if (number < 0)
+    Console.Write($" {number} не натуральное число ");
+    if ( number == 0)
+    {
+        return;
+    }
+    Console.Write(" " + number);
+    PrintNumberRevers (number - 1);
+}
+PrintNumberRevers(number);
