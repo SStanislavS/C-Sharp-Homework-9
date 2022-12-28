ae7a13d2add7bf75f14008241c124ea753408b8c
Stanislav <S@mail.ru>
2022-12-28 17:22:27 +0300

    add Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программа заполняет спирально массив 4 на 4.
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[n, n];
+
+int num = 1; int i = 0; int j = 1;
+
+while (num < n * n)
+{
+    arr[i, j] = num;
+    if (i <= j +1 && i + j < n -1)
+        ++j;
+    else if (i < j && i +j >= n - 1)
+        ++i;
+    else if (i >= j && i + j > n -1)
+        --j;
+    else 
+        --i;
+    ++num;
+}
+
+PrintArr(arr);
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
